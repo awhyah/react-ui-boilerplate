c28158b545e24c77e890ea458f16ac3aa35c2636
awhyah <avvhyah@gmail.com>
2019-04-24 17:49:28 +0300

    Add style loading

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React    from 'react';
 import ReactDOM from 'react-dom';
 
+import './index.less';
+
 function Index() {
     return (
         <h1>Index1</h1>
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,14 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'less-loader'
+                ]
             }
         ]
     },
