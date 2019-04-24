e0230b08e3c729bb694b08723c1649da940d25ef
awhyah <avvhyah@gmail.com>
2019-04-24 16:04:26 +0300

    Set up webpack-dev-server

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React    from 'react';
+import ReactDOM from 'react-dom';
+
+function Index() {
+    return (
+        <h1>Index1</h1>
+    )
+}
+
+ReactDOM.render(<Index />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,8 @@ module.exports = {
                 }
             }
         ]
+    },
+    devServer: {
+        contentBase: './dist'
     }
 }
